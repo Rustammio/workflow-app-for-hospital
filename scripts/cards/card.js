0e876f8dc72a3203ee6e713b39e22c28abcebb42
Татьяна Яцкова <t.yatskova@gmail.com>
2022-08-02 18:31:14 +0300

    created file package.json, fix stucture script, add data to gitignore

--- scripts/cards/card.js
+++ scripts/cards/card.js
@@ -1,1 +1,2 @@
-import Card from '.././classes/class_card.js'
+import Card from '../classes/class_card/main_class_card.js'
+import DeleteCard  from '../classes/class_card/card_delete.js'
---
+++ scripts/classes/class_card/card_delete.js
@@ -0,0 +1,6 @@
+import Card from "./main_class_card.js"
+export default class DeleteCard extends Card {
+    constructor(){
+        super()
+    }
+}
