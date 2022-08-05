105c3628574dd220f86a6d5c265abeef206ab383
Татьяна Яцкова <t.yatskova@gmail.com>
2022-08-06 00:15:17 +0300

    create class Modal

--- scripts/classes/class_modal/main_class_modal.js
+++ scripts/classes/class_modal/main_class_modal.js
@@ -1,5 +1,50 @@
 export default class Modal{
- constructor(){
+ constructor( title){
+     this.title = title;
+
+     this.modalElement = document.createElement('div');
+     this.modalHeaderWrapper = document.createElement('div');
+     this.modalTitle = document.createElement('h5')
+     this.closeModalButton = document.createElement('button')
+     this.contentWrapper = document.createElement('div');
+     this.buttonWrapper = document.createElement('div');
+     this.modalNoButton = document.createElement('button');
+     this.modalYesButton = document.createElement('button');
 
  }
+    createElements(container){
+        this.modalElement.classList.add("container-md")
+        this.modalElement.classList.add("modal-content")
+
+        this.modalTitle.innerText = `${this.title}`
+        this.modalTitle.classList.add("modal-title")
+        this.modalHeaderWrapper.append(this.modalTitle)
+
+        this.closeModalButton.classList.add("close-button")
+        this.modalHeaderWrapper.append(this.closeModalButton)
+
+        this.modalElement.append(this.modalHeaderWrapper)
+        this.modalElement.append(this.contentWrapper)
+
+        this.modalNoButton.classList.add("btn")
+        this.modalYesButton.classList.add("btn-light")
+
+        this.buttonWrapper.append(this.modalNoButton)
+        this.buttonWrapper.append(this.modalYesButton) 
+        this.modalElement.append(this.buttonWrapper)
+
+        // createElements()
+        container.append(this.modalElement)
+
+        }
+
+        // render(container){
+           
+        // }
+
+
+        
+        
+    
 }
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,11 @@
 import {logIn} from "./api/log_in.js";
+import Modal from "./classes/class_modal/main_class_modal.js";
 
 const sign = document.querySelector('.sign')
+
 sign.addEventListener('click', logIn)
+
+
+const modalContainer = document.querySelector(".modal-dialog")
+const testModal = new Modal("create visit").createElements(modalContainer)
+// testModal.render()
