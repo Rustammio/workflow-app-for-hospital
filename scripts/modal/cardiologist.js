33cc1c96492e95f93df8804d906e630d7fe551ef
Татьяна Яцкова <t.yatskova@gmail.com>
2022-08-07 23:07:54 +0300

    add scrollbar

--- scripts/modal/cardiologist.js
+++ scripts/modal/cardiologist.js
@@ -5,13 +5,15 @@ export const createCardiologistInstance = () =>{
   const cardioInstance = document.querySelector(".form_footer")
   
   cardioInstance.insertAdjacentHTML("beforeBegin", `
-  <div class="input-group mb-3">
+  <div class="row g-3">
+  <div class="input-group col mb-3">
   <input type="text" class="form-control" placeholder="normal pressure" aria-label="normal pressure" aria-describedby="basic-addon2">
   <span class="input-group-text" id="basic-addon2">normal pressure</span>
 </div>
-<div class="input-group mb-3">
+<div class="input-group col mb-3">
 <input type="text" class="form-control" placeholder="body mass index" aria-label="body mass index" aria-describedby="basic-addon2">
 <span class="input-group-text" id="basic-addon2">body mass index</span>
+</div>
 </div>
 <div class="input-group mb-3">
 <input type="text" class="form-control" placeholder="past diseases of the cardiovascular system" aria-label="past diseases of the cardiovascular system" aria-describedby="basic-addon2">
