86b33b5d39200bb8c3fc006caf572f65e2f4292b
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-08-13 09:06:41 +0300

    no-cards-to-show-added

--- scripts/api/get_cards.js
+++ scripts/api/get_cards.js
@@ -1,4 +1,5 @@
 import createCard from "../util/create_card.js";
+import {cardContainer} from "./log_in.js";
 export function cardsManage() {
     fetch("https://ajax.test-danit.com/api/v2/cards", {
         headers: {
@@ -9,7 +10,20 @@ export function cardsManage() {
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
-            data.forEach((el)=> createCard(el))
+            // data =[]
+            if (data.length > 0){
+                 data.forEach((el)=> createCard(el))
+            }else{
+                    cardContainer.insertAdjacentHTML('afterbegin',`
+                    <div class="container">
+                    <div class="animate one">
+                    <span>N</span><span>o</span>&nbsp;<span>c</span><span>a</span><span>r</span><span>d</span><span>s</span>&nbsp;<span>t
+                </span><span>o</span>&nbsp;<span>s</span><span>h</span><span>o
+                </span><span>w
+                </span><span>!</span>
+                </div></div>`)
+
+            }
             localStorage.removeItem("data");
             // localStorage.setItem("data", JSON.stringify(arrayOfCards));
             localStorage.setItem("data", JSON.stringify(data));
