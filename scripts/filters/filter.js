adc2bc3406828051029811f4cceb0ea10b3fdfa4
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-08-13 19:26:27 +0300

    reset-and-search-func-fixed

--- scripts/filters/filter.js
+++ scripts/filters/filter.js
@@ -1,5 +1,6 @@
 // import createCard from "../render/createCard.js";
 import { cardContainer } from "../api/log_in.js";
+import createCard from "../util/create_card.js";
 const urgencyInp = document.querySelector(".urgency");
 // console.log(urgencyInp);
 // urgencyInp.addEventListener("change", () => {
@@ -14,49 +15,35 @@ const textSearch = document.querySelector(".textSearch");
 export const doctorType = document.querySelector(".doctorType");
 export default function filtration() {
     const data = JSON.parse(localStorage.getItem("data"));
-    // console.log("filtraton output :", data);
-    // console.log(statusInp.value);
-    // console.log(urgencyInp.value);
-    // console.log(textSearch.value);
-    const fiterd = data.filter((el) => {
+    const filterd = data.filter((el) => {
         if (
-            (el.status.toLowerCase() == statusInp.value.toLowerCase() &&
-                el.urgency.toLowerCase() == urgencyInp.value.toLowerCase()) ||
-            (statusInp.value == "" &&
-                el.urgency.toLowerCase() == urgencyInp.value.toLowerCase()) ||
-            (urgencyInp.value == "" &&
-                el.status.toLowerCase() == statusInp.value.toLowerCase()) ||
-            (statusInp.value == "" && urgencyInp.value == "")
+            (el.status.toLowerCase() === statusInp.value.toLowerCase() &&
+                el.urgency.toLowerCase() === urgencyInp.value.toLowerCase()) ||
+            (statusInp.value === "" &&
+                el.urgency.toLowerCase() === urgencyInp.value.toLowerCase()) ||
+            (urgencyInp.value === "" &&
+                el.status.toLowerCase() === statusInp.value.toLowerCase()) ||
+            (statusInp.value === "" && urgencyInp.value === "")
         ) {
             if (textSearch.value === "") {
                 return el;
             } else if (textSearch) {
                 if (
-                    el.title.includes(textSearch.value) ||
-                    el.body.includes(textSearch.value)
+                    el.name.includes(textSearch.value) ||
+                    el.description.includes(textSearch.value)
                 ) {
                     return el;
                 }
             }
         }
     });
-    console.log(fiterd);
+    console.log(filterd);
 
     cardContainer.innerHTML = "";
-    if (fiterd.length === 0){
+    if (filterd.length === 0){
         cardContainer.insertAdjacentHTML("afterbegin", `
         <h5 class="defText">There are no such cards</h5>
         `)
     }
-    // fiterd.forEach(({ status, body, doctor, title, urgency, id }) => {
-    //     return new createCard(
-    //         status,
-    //         body,
-    //         doctor,
-    //         title,
-    //         urgency,
-    //         id
-    //     ).render();
-        // console.log(status, bp, body, doctor, title, weight);
-    // });
+    filterd.forEach((el)=> createCard(el))
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,17 @@
 import{logIn} from "./api/log_in.js";
 import {selectedOption} from './modal/select_modal.js'
 import {getDataForm} from "./modal/get_data_form.js"
-
+import filtration from "./filters/filter.js";
+import {reset} from "./filters/reset.js";
 const sign = document.querySelector('.sign')
-const signIn = document.querySelector("#signIn");
-
+const search = document.querySelector(".search");
+search.addEventListener('click', filtration)
+const resetBtn = document.querySelector('.reset')
+resetBtn.addEventListener('click',reset)
 sign.addEventListener('click', logIn)
 dragula([document.querySelector(".card-container")]);
-
-
 selectedOption()
 getDataForm()
+window.addEventListener('load',()=>{
+    localStorage.clear()
+})
