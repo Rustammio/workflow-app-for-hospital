5e76beba0109ab69558f3470570a7791e02d6aeb
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-08-14 17:35:11 +0300

    filter-bug-fixing

--- scripts/filters/filter.js
+++ scripts/filters/filter.js
@@ -1,6 +1,7 @@
 // import createCard from "../render/createCard.js";
-import { cardContainer } from "../api/log_in.js";
+import {cardContainer} from "../api/log_in.js";
 import createCard from "../functions/util/create_card.js";
+
 const urgencyInp = document.querySelector(".urgency");
 // console.log(urgencyInp);
 // urgencyInp.addEventListener("change", () => {
@@ -17,13 +18,14 @@ export default function filtration() {
     const data = JSON.parse(localStorage.getItem("data"));
     const filterd = data.filter((el) => {
         if (
+            (statusInp.value === "" && urgencyInp.value === "") ||
             (el.status === statusInp.value.toLowerCase() &&
                 el.urgency.toLowerCase() === urgencyInp.value.toLowerCase()) ||
             (statusInp.value === "" &&
                 el.urgency.toLowerCase() === urgencyInp.value.toLowerCase()) ||
             (urgencyInp.value === "" &&
-                el.status === statusInp.value.toLowerCase()) ||
-            (statusInp.value === "" && urgencyInp.value === "")
+                el.status === statusInp.value.toLowerCase())
+
         ) {
             if (textSearch.value === "") {
                 return el;
@@ -36,14 +38,23 @@ export default function filtration() {
                 }
             }
         }
+        if (textSearch && statusInp.value === "" && urgencyInp.value === "") {
+            if ((el.name === textSearch.value) ||
+                (el.description === textSearch.value) ||
+                (el.name.includes(textSearch.value) ||
+                    el.description.includes(textSearch.value)))
+            {
+                return el;
+            }
+        }
     });
     console.log(filterd);
 
     cardContainer.innerHTML = "";
-    if (filterd.length === 0){
+    if (filterd.length === 0) {
         cardContainer.insertAdjacentHTML("afterbegin", `
         <h5 class="defText">There are no such cards</h5>
         `)
     }
-    filterd.forEach((el)=> createCard(el))
+    filterd.forEach((el) => createCard(el))
 }
